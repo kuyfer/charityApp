780a6e47e023ce3853a4d170acf7658084be2383
Mhimer El mehdi <mmhimer0@gmail.com>
2025-03-17 10:18:34 +0100

    okey

---
+++ src/test/java/ma/emsi/charityApp/CharityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.charityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
